ef6045ffed570f66aa52d168295da7590feac402
Julia Debelova <u.deb@mail.ru>
2023-11-09 17:06:11 +0500

    edit after check

--- Editor3D/src/ModelElements/Scene.java
+++ Editor3D/src/ModelElements/Scene.java
@@ -3,16 +3,45 @@ package ModelElements;
 import java.util.List;
 
 public class Scene {
-    public Integer Id;
+    public int ID;
     public List<PoligonalModel> Models;
     public List<Flash> Flashes;
+    public List<Camera> cameras;
 
-    /* @todo: Что имели ввиду на UML под этими Type?
-    public Type method1(Type type) {
-
+    public Scene(int id, List<PoligonalModel> models, List<Flash> flashes, List<Camera> cameras) throws Exception {
+        this.ID = id;
+        if (models.size() > 0) {
+            this.Models = models;
+        } else {
+            throw new Exception("Должна быть одна модель");
+        }
+        this.Flashes = flashes;
+        if (cameras.size() > 0) {
+            this.cameras = cameras;
+        } else {
+            throw new Exception("Должна быть одна камера");
+        }
     }
-
-    public Type method2(Type type1, Type type2) {
-
-    }*/
+    
+    public <T> T method1(T flash) {
+        return flash;
+    }
+    
+    public <T, E> T method2(T Model, E Flash) {
+        return Model;
+    }
+    
+    // /**
+    // * заглушка
+    // */
+    // public Type1 method1(Type1 t) {
+    // return t;
+    // }
+    
+    // /**
+    // * заглушка
+    // */
+    // public Type1 method1(Type1 t1, Type2 t2) {
+    // return t1;
+    // }
 }
